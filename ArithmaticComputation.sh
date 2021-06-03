7340e67d036f6ec0efc7a08baa762a128753b8b0
DKUMAR196 <deepakmecet@gmail.com>
2021-06-03 18:33:28 +0530

    Fourth Operation

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -2,6 +2,6 @@ echo "Enter a, b and c"
 read a b c
       echo "values of a= $a, b= $b and c+ $c"
 
-   S1=$(($a*$b+$c))
+   S1=$(($c+$a/$b))
 
-       echo "$a*$b+$c= $S1"
+       echo "$c+$a/$b= $S1"
